ea79c8bb528eedfe6166af044bff9b8f13784812
m3t4l <daniel.aloisio1@gmail.com>
2026-01-16 21:58:14 -0300

    Alter CMakeLists.txt

--- src/i_main.c
+++ src/i_main.c
@@ -25,10 +25,10 @@ static const char
 rcsid[] = "$Id: i_main.c,v 1.4 1997/02/03 22:45:10 b1 Exp $";
 
 
-//#include "doomdef.h"
+#include "doomdef.h"
 
 #include "m_argv.h"
-//#include "d_main.h"
+#include "d_main.h"
 
 int
 main
@@ -38,7 +38,7 @@ main
     myargc = argc;
     myargv = argv;
  
-    //D_DoomMain ();
+    D_DoomMain ();
 
     return 0;
 } 
